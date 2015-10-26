f4c8e0f96d0848adf94f584bffe474a6e22c9504
Jagjit Singh <singhjag@dei.unipd.it>
2015-10-26 23:52:07 +0100

    Hello World created.

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jagjit Singh Bal
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
